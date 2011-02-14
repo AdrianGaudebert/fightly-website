697fa9e921cfb8c17113e31aee03fc0220bdd5f4
AdrianGaudebert <adrian@gaudebert.fr>
2011-02-14 18:09:42 +0100

    First commit.

---
+++ app/config.php
@@ -0,0 +1,34 @@
+<?php
+
+Atomik::set(array (
+  'app' =>
+  array (
+    'layout' => '_layout',
+    'default_action' => 'home',
+    'views' =>
+    array (
+      'file_extension' => '.phtml',
+    ),
+  ),
+  'atomik' =>
+  array (
+    'start_session' => true,
+    'class_autoload' => true,
+    'trigger' => 'action',
+    'catch_errors' => true,
+    'display_errors' => true,
+    'debug' => false,
+    'url_rewriting' => true,
+  ),
+  'styles' =>
+  array (
+    0 => 'assets/css/style.css',
+    1 => 'assets/css/main.css',
+  ),
+  'plugins' =>
+  array (
+  ),
+  'scripts' =>
+  array (
+  ),
+));
